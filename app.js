eaefe0594e593c091a7077182ad2e8496b42bd3c
tiendung004 <dungptph36187@fpt.edu.vn>
2025-07-23 00:15:40 +0700

    deploy lần 10

--- app.js
+++ app.js
@@ -101,6 +101,12 @@ const io = new Server(server, {
     credentials: true,
   },
 });
+
+const PORT = process.env.PORT || 4000
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
 // Lắng nghe kết nối từ client
 
 const User = require('./models/User'); // Import model User
